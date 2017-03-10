5c97861acffc8ae369f0a97ab956a0936c8f9893
Grant Richmond <mail@grant.codes>
2017-03-10 11:58:08 +0100

    move files to src

---
+++ src/dependencies-browser.js
@@ -0,0 +1,5 @@
+// export Fetch;
+export {parse as qsParse, qsStringify} from 'qs';
+// export FormData;
+export {default as Microformats} from 'microformat-shiv';
+export {default as objectToFormData} from './lib/object-to-form-data';
