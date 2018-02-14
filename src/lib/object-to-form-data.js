e3f179020d8b1f162bb9c3e9e4fd971a3d54ad16
Grant Richmond <mail@grant.codes>
2018-02-14 10:03:30 -0600

    improve how objectToFormData function handles objects and arrays

--- src/lib/object-to-form-data.js
+++ src/lib/object-to-form-data.js
@@ -11,6 +11,12 @@ export default function objectToFormData(
       key = name + '[' + key + ']';
     }
     if (Array.isArray(data)) {
+      data.forEach(arrayItem => {
+        const arrayData = {};
+        arrayData[key + '[]'] = arrayItem;
+        formData = objectToFormData(arrayData, formData);
+      });
+    } else if (data !== null && typeof data === 'object') {
       formData = objectToFormData(data, formData, key);
     } else {
       formData.append(key, data);
