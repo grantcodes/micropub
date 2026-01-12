a02a1a6d9a92e258a1fcc26f8b823403e6e70a45
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-01-12 18:16:26 +0800

    fix(bug): add `g` to first regex in order to replace all instances of `+` instead of just the first

--- src/lib/base64url.ts
+++ src/lib/base64url.ts
@@ -2,7 +2,7 @@ export function base64UrlEncode(bytes: Uint8Array): string {
 	const binaryString = String.fromCharCode(...bytes)
 
 	return btoa(binaryString)
-		.replace(/\+/, "-")
+		.replace(/\+/g, "-")
 		.replace(/\//g, "_")
 		.replace(/=+$/g, "")
 }
